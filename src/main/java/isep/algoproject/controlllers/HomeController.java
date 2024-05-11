8009e3b8736a958f56271eefca48edf752aa3c24
lenaf0n <lenafondeur96@gmail.com>
2024-05-11 02:17:49 +0200

    login/signup front

--- src/main/java/isep/algoproject/controlllers/HomeController.java
+++ src/main/java/isep/algoproject/controlllers/HomeController.java
@@ -1,12 +1,41 @@
 package isep.algoproject.controlllers;
 
+import isep.algoproject.models.User;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
     public String home() {
         return "index";
+    }
+
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("user", new User());
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String loginSubmit(@ModelAttribute("user") User user, HttpSession session) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+
+    @GetMapping("/signup")
+    public String signup(Model model) {
+        model.addAttribute("user", new User());
+        return "signup";
+    }
+
+    @PostMapping("/signup")
+    public String signupSubmit(@ModelAttribute("user") User user) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 }
